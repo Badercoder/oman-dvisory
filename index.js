55f968e7656dfed2360d1ce1b95bbba4e8b120d7
Badercoder <baders-lastcard@hotmail.com>
2023-06-04 20:12:08 +0400

    updated the javascript code for nav menu

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-document.getElementById("menuIcon").addEventListener("click", function () {
+// Toggle the menu visibility on clicking the menu icon
+document.getElementById("menuIcon").addEventListener("click", function (event) {
   const menu = document.getElementById("menu");
 
   if (menu.classList.contains("hide")) {
     menu.classList.remove("hide");
   } else {
+    menu.classList.add("hide");
+  }
+
+  // Stop event propagation to prevent triggering of the window click event below
+  event.stopPropagation();
+});
+
+// Close the menu if a click is detected anywhere else in the window
+window.addEventListener("click", function (event) {
+  const menu = document.getElementById("menu");
+
+  // If the click event's target is not inside the menu, hide the menu
+  if (!menu.contains(event.target) && !menu.classList.contains("hide")) {
+    menu.classList.add("hide");
+  }
+});
+
+// Close the menu if the 'Escape' key is pressed
+window.addEventListener("keydown", function (event) {
+  const menu = document.getElementById("menu");
+
+  if (event.key === "Escape" && !menu.classList.contains("hide")) {
     menu.classList.add("hide");
   }
 });
